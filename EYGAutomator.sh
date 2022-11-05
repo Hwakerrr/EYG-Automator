aa0bc2e771b3e4a855c9a433b7d3c4c0f1fc4c67
yathu <yathugk@gmail.com>
2022-11-05 19:15:47 -0400

    Changes:- Necessary flags added -w for dirb, -sS remove from nmap because of root privilege issue in wsl, --unprivileged flag notification added. Look & feel - banner added, usage printed

--- EYGAutomator.sh
+++ EYGAutomator.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 #by yathu.g.krishna
-printf "${NC}\n"
-printf "${NC}\n"
-printf "${NC}\n" 
 
 # Define ANSI color variables
 RED='\033[0;31m'
@@ -10,41 +7,57 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 origIFS="${IFS}"
+echo "[+]Usage:- EYGAutomator.sh victim.com"
+echo "[+]Add --unprivileged flag if after victim.com for internal application."
+echo "[+]example:- EYGAutomator.sh victim.com --unprivileged"
+printf "${YELLOW}===========================================>START<===========================================\n"
+printf "${NC}\n"
+
+
+echo "\033[0;32m     | __|\ \ / // __|  ___    /_\ | | | ||_   _|/ _ \ |  \/  |  /_\|_   _|/ _ \ | _ \+\n     | _|  \ V /| (_ | |___|  / _ \| |_| |  | | | (_) || |\/| | / _ \ | | | (_) ||   /+\n     |___|  |_|  \___|       /_/ \_\____/   |_|  \___/ |_|  |_|/_/ \_\|_|  \___/ |_|_\;"  
+
+
+ 
+
+
 
 export mynmap="/mnt/c/Program Files (x86)/Nmap/nmap.exe"
 
+printf "${NC}\n"
+printf "${YELLOW}===========================================>START<===========================================\n"
+printf "${NC}\n"
 printf "${YELLOW}=======================================>(Frontend Controls-11.5,16.4)<=======================================>\n"
 printf "${GREEN}---------------------Default Port Scan---------------------\n"
 printf "${NC}\n"
 
-"${mynmap}" -sS -sV $1
+"${mynmap}" -sV $2 $1
 
 printf "${NC}\n"
 printf "${GREEN}---------------------Full Port Scan---------------------\n"
 printf "${NC}\n"
 
-"${mynmap}" -p0-65535 $1
+"${mynmap}" -p0-65535 $2 $1
 
 printf "${NC}\n"
 printf "${YELLOW}=======================================>(Frontend Controls-11.8)<=======================================\n"
 printf "${GREEN}---------------------Insecure ports FTP(21),Telnet(23),SSH(22),RemoteShell(514) check---------------------\n"
 printf "${NC}\n"
 
-"${mynmap}" -p21,23,22,514 -sV $1
+"${mynmap}" -p21,23,22,514 -sV $2 $1
 
 printf "${NC}\n"
 printf "${YELLOW}=======================================>(Frontend Controls-11.9)<=======================================\n"
 printf "${GREEN}---------------------Vulnerable SMB(139,445) version check---------------------\n"
 printf "${NC}\n"
 
-"${mynmap}" -p139,445 --script smb-protocols $1
+"${mynmap}" -p139,445 --script smb-protocols $2 $1
 
 printf "${NC}\n"
 printf "${YELLOW}=======================================>(Frontend Controls-10.2)<=======================================\n"
 printf "${GREEN}--------------------TLS certificate check---------------------\n"
 printf "${NC}\n"
 
-"${mynmap}" --script ssl-cert -p443 $1
+"${mynmap}" --script ssl-cert -p443 $2 $1
 
 printf "${NC}\n"
 printf "${GREEN}---------------------Sending request via HTTP---------------------\n"
@@ -61,18 +74,18 @@ printf "${YELLOW}=======================================>(Frontend Controls-16.1
 printf "${GREEN}----------------------Weak TLS cipher check----------------------\n"
 printf "${NC}\n"
 
-"${mynmap}" --script ssl-enum-ciphers -p443 $1
+"${mynmap}" --script ssl-enum-ciphers -p443 $2 $1
 
 printf "${NC}\n"
 printf "${YELLOW}=======================================>(Frontend Controls-14.3,16.15)<=======================================\n"
 printf "${GREEN}---------------------Unauthenticated directory enumeration---------------------\n"
 printf "${NC}\n"
 
-dirb https://$1 common.txt
+dirb -w https://$1 common.txt
 
-dirb https://$1 big.txt
+dirb -w https://$1 big.txt
 
-#dirb https://$1 $2
+#dirb https://$1 $3
 
 printf "${NC}\n"
 printf "${YELLOW}=======================================>(Frontend Controls-11.6,14.7,16.14)<=======================================\n"
@@ -87,3 +100,6 @@ printf "${GREEN}---------------------Response Headers---------------------\n"
 printf "${NC}\n"
 
 curl -k -X OPTIONS -i https://$1
+
+printf "${NC}\n"
+printf "${YELLOW}=======================================>END<=======================================\n"
